2fb5c24a5b5eef116dcb1e773ea5f6c2a0061215
Faiznurullah <faizn103a@gmail.com>
2025-12-11 07:19:53 +0700

    Structure text appbar and Card Container

--- lib/detail_page.dart
+++ lib/detail_page.dart
@@ -1,30 +1,59 @@
 import 'package:flutter/material.dart';
 
 class DetailPage extends StatelessWidget {
-
   final String imageUrl;
   final String name;
   final String type;
 
-  const DetailPage({super.key, required this.imageUrl, required this.name, required this.type});
+  const DetailPage({
+    super.key,
+    required this.imageUrl,
+    required this.name,
+    required this.type,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-     appBar: AppBar(
-      title: Text(name),
-      centerTitle: true,
-     ),
-     body: SafeArea(
-      child: Padding(
-        padding: const EdgeInsets.all(18),
-        child: Column(
-          children: [
-           
-          ],
+      appBar: AppBar(title: Column(
+        children: [
+          Text(name),
+          Text(type, style: TextStyle(fontSize: 12),)
+        ],
+      ), centerTitle: true),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(18),
+          child: Column(
+            children: [
+              SizedBox(height: 20),
+              Container(
+                decoration: BoxDecoration(
+                  color: type == 'Fire'
+                      ? Colors.red[100]
+                      : type == 'Water'
+                      ? Colors.blue[100]
+                      : type == 'Grass'
+                      ? Colors.green[100]
+                      : Colors.grey[200],
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Column(
+                  children: [
+                    Image.network(
+                      imageUrl,
+                      width: double.infinity,
+                      fit: BoxFit.fill,
+                    ),
+                    SizedBox(height: 5),
+                   
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
-      )
-     ),
+      ),
     );
   }
 }
