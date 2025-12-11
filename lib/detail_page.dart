a03ef15ba9c55e4bb0183af653fb8ccfbe365bb9
Faiznurullah <faizn103a@gmail.com>
2025-12-11 07:05:02 +0700

    Attribute Constructor DetailPage

--- lib/detail_page.dart
+++ lib/detail_page.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 
 class DetailPage extends StatelessWidget {
-  const DetailPage({super.key});
+
+  final String imageUrl;
+  final String name;
+  final String type;
+
+  const DetailPage({super.key, required this.imageUrl, required this.name, required this.type});
 
   @override
   Widget build(BuildContext context) {
